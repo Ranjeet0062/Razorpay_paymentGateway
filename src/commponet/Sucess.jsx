d1bf5deaa610b138981f3ff5df311e3a11102516
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-03-02 17:03:25 +0530

    sucess page completed

--- src/commponet/Sucess.jsx
+++ src/commponet/Sucess.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Sucess() {
+  const navigate = useNavigate()
   return (
-    <div className='text-black'>Sucess</div>
+    <>
+      <div className='text-black'>your order is Sucessfully completed  </div>
+      <button type='button' className='text-white bg-green-800 p-3 rounded-lg font-semibold' onClick={() => navigate("/home")}>Go to Home</button>
+    </>
   )
 }
 
